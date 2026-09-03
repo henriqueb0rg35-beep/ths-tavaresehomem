2e9a27a1d9f85a036755ec50bbfd7f031180a900
henriqueb0rg35-beep <henriqueb0rg35@gmail.com>
2026-09-03 13:06:59 -0300

    Update main.js
    // Lógica de simulação de terminal
    const textArray = "Arquitetura de Software & Manutenção.";
    const typingElement = document.querySelector('.typing-text');
    let index = 0;
    
    function typeWriter() {
        if (index < textArray.length) {
            typingElement.innerHTML += textArray.charAt(index);
            index++;
            setTimeout(typeWriter, 100); // Controla a velocidade de digitação em milissegundos
        }
    }
    
    // Inicia a função assim que a janela carrega
    window.onload = typeWriter;

--- THS-TavaresEHomemSystems/js/main.js
+++ THS-TavaresEHomemSystems/js/main.js
@@ -1,1 +1,15 @@
+// Lógica de simulação de terminal
+const textArray = "Arquitetura de Software & Manutenção.";
+const typingElement = document.querySelector('.typing-text');
+let index = 0;
 
+function typeWriter() {
+    if (index < textArray.length) {
+        typingElement.innerHTML += textArray.charAt(index);
+        index++;
+        setTimeout(typeWriter, 100); // Controla a velocidade de digitação em milissegundos
+    }
+}
+
+// Inicia a função assim que a janela carrega
+window.onload = typeWriter;
